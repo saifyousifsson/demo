4df534972e602f19c4077899ffce2600e908938d
saifyousifsson <saif.yousifsson@gmail.com>
2021-12-19 19:43:56 +0100

    csharUppgift

---
+++ source/repos/csharpuppgift/customerList/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using customerList.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace customerList
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private static List<ContactPerson> contacts = new List<ContactPerson>();	
+
+	  public object WriteTextFile { get; private set; }
+	  public object FutureAccessList { get; private set; }
+
+	  public MainPage()
+        {
+            this.InitializeComponent();
+
+        }
+        private async void btnAdd_Click(object sender, RoutedEventArgs e)
+            {
+		var savePicker = new Windows.Storage.Pickers.FileSavePicker();
+		savePicker.SuggestedStartLocation =
+		 Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+		
+		savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".txt" });
+		
+		savePicker.SuggestedFileName = "New Document";
+
+		if (!string.IsNullOrEmpty(tbFirstName.Text) && !string.IsNullOrEmpty(tbLastName.Text) && !string.IsNullOrEmpty(tbEmail.Text))
+		{
+
+		    contacts.Add(new ContactPerson
+		    {
+			  Rabattkod = Guid.NewGuid(),
+			  FirstName = tbFirstName.Text,
+			  LastName = tbLastName.Text,
+			  Email = tbEmail.Text,
+			 // RabattKod = tbRabattkod.Text ??""
+
+		    }
+		);
+		    lvContacts.ItemsSource = new List<Object>();
+		   
+		    lvContacts.ItemsSource = contacts;
+		
+		    FileOpenPicker openPicker = new FileOpenPicker();
+		    openPicker.ViewMode = PickerViewMode.Thumbnail;
+		    openPicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+		    openPicker.FileTypeFilter.Add( ".txt");
+		  // StorageFile file = await openPicker.PickSingleFileAsync();
+		   StorageFile file = await savePicker.PickSaveFileAsync();
+		    CachedFileManager.DeferUpdates(file);
+		    await Windows.Storage.FileIO.AppendTextAsync(file, $"{tbFirstName.Text}, {tbLastName.Text},{ tbEmail.Text},\r\n");  
+		}
+	  }
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var x = lvContacts.SelectedItem;
+            lvContacts.SelectedItem = null;
+            contacts.Remove(x as ContactPerson);
+            lvContacts.ItemsSource = null;
+            lvContacts.ItemsSource = contacts;
+
+        }
+
+    }
+
+	  }
+        
+    
+    
+    
+    
+    
+
---
+++ source/repos/csharpuppgift/customerList/Models/ContactPerson.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace customerList.Models
+{
+    public class ContactPerson
+    {
+	  public Guid Rabattkod { get; set; }
+
+	  public string FirstName { get; set; }
+	  public string LastName { get; set; }
+	  public string Email { get; set; }
+	  public string RabattKod { get; set; }
+	  public object FileHelper { get;  set; }
+	  public string FullName => $"{FirstName} {LastName}";
+	  public string Guid => RabattKod.ToString().Substring(0,5);
+	  public IDictionary<string, IList<string>> FileTypeChoices { get; }
+	  public StorageFolder LocalFolder { get; }
+	  public PickerViewMode ViewMode { get; set; }
+	  public IList<string> FileTypeFilter { get; }
+    }
+
+}
